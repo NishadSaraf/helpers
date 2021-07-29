f5f4012b715a52f001542192ec1918d2e0ac27da
Nishad Saraf <nishadsaraf@gmail.com>
2021-07-28 20:14:05 -0700

    Add helper script to create commit markdowns
    Add helper script that fetches GIT repos and creates commit markdown
    files.
    
    Signed-off-by: Nishad Saraf <nishadsaraf@gmail.com>

---
+++ archive_commits.sh
@@ -0,0 +1,62 @@
+author="nishad.saraf"
+commits_dir=${PWD}/commits
+repos=( "embeddedsw"
+	"linux-xlnx"
+	"plnx-aie-examples" "plnx-aie-examples" "plnx-aie-examples"
+)
+branches=("master"
+	  "master"
+	  "rel-v2020.1" "rel-v2020.2" "rel-v2021.1"
+)
+
+pushd () {
+	command pushd "$@" > /dev/null
+}
+
+popd () {
+	command popd "$@" > /dev/null
+}
+
+create_commit_mds () {
+	message="---%n"
+	message+="date: '%as'%n"
+	message+="title: '%s'%n"
+	message+="github: 'https://github.com/Xilinx/${1}/commit/%H'%n"
+	message+="external: ''%n"
+	message+="tech:%n- C%n"
+	message+="company: 'Xilinx'%n"
+	message+="showInProjects: false%n"
+	message+="---"
+
+	test -d ${1} || git clone https://github.com/Xilinx/${1}.git
+	echo "[INFO]: Entering ${PWD}/${1} directory"
+	pushd ${1}
+	echo "[INFO]: Fetching commits from ${2} branch of ${1} repo"
+	git fetch origin
+	git reset --hard origin/${2}
+	git --no-pager log --author=${author} --pretty="format:${message}" >   \
+	    ${commits_dir}/log
+	echo "[INFO]: Leaving ${PWD} directory"
+	popd
+	pushd ${commits_dir}
+	split -d -l 10 log --additional-suffix=.md ${1}_${2}_
+	rm log
+	echo "[INFO]: Leaving ${PWD} directory"
+	popd
+}
+
+main () {
+	rm -rf ${commits_dir}
+	mkdir ${commits_dir}
+	mkdir -p repo
+	echo "[INFO]: Entering ${PWD}/repo directory"
+	pushd repo
+	for ((r = 0; r < ${#repos[@]}; r++ ));
+	do
+		create_commit_mds ${repos[$r]} ${branches[$r]}
+	done
+	echo "[INFO]: Leaving ${PWD} directory"
+	popd
+}
+
+main
